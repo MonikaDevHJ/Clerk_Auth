819e008d117b443dde76b6742014ac24d145e9a1
Monika H J <monikahj7@gmail.com>
2026-02-10 20:09:05 +0530

    clerk auth is done

--- app/dashboard/page.tsx
+++ app/dashboard/page.tsx
@@ -1,3 +1,16 @@
-export default function DashboardPage() {
-  return <h1>Dashboard - Only Logged In Users</h1>;
+import { auth } from "@clerk/nextjs/server";
+import { redirect } from "next/navigation";
+
+export default async function DashboardPage() {
+  const { userId } = await auth();
+
+  if (!userId) {
+    redirect("/sign-in");
+  }
+
+  return (
+    <div>
+      <h1>Dashboard - Only Logged In Users</h1>
+    </div>
+  );
 }
